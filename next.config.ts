6dd3e7a28d79202828b1747ed56268b5e29289b2
Bernadett Novak <bernadett.novak@adnovum.hu>
2026-01-12 16:27:26 +0100

    host added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const r2PublicUrl = process.env.R2_PUBLIC_URL;
+let r2Hostname: string | undefined;
+
+if (r2PublicUrl) {
+  try {
+    r2Hostname = new URL(r2PublicUrl).hostname;
+  } catch {
+    r2Hostname = undefined;
+  }
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: r2Hostname ? [r2Hostname] : [],
+  },
 };
 
 export default nextConfig;
